0bce1fbdcadcc5a7602e56145de0de1a2afd5ce1
kianoushmahboob <kianoushmhaboob@gmail.com>
2019-06-19 18:42:01 +0430

    change status nav

--- js/app.js
+++ js/app.js
@@ -12,18 +12,22 @@ function closeNav() {
 $(".color-1").click(function () {
   $(".navbar").css('background-image', 'linear-gradient(to right , #7b24ebba, #ab22dab5, #ca2fcac7, #df44bdc7, #ed5bb3c2)');
   $(".sidenav-color").css('background-image', 'linear-gradient(to left bottom , #7b24ebba, #ab22dab5, #ca2fcac7, #df44bdc7, #ed5bb3c2)');
+  $(".bg-color-statusCard").css('background-image', 'linear-gradient(to right  , #7b24ebba, #ab22dab5, #ca2fcac7, #df44bdc7, #ed5bb3c2)');
 });
 $(".color-2").click(function () {
   $(".navbar").css('background-image', 'linear-gradient(to right, rgba(5, 25, 55, 0.74), rgba(0, 77, 122, 0.7), rgba(0, 135, 147, 0.71), rgba(0, 191, 114, 0.7), rgba(168, 235, 18, 0.7)');
   $(".sidenav-color").css('background-image', 'linear-gradient(to left bottom, rgba(5, 25, 55, 0.74), rgba(0, 77, 122, 0.7), rgba(0, 135, 147, 0.71), rgba(0, 191, 114, 0.7), rgba(168, 235, 18, 0.7)');
+  $(".bg-color-statusCard").css('background-image', 'linear-gradient(to right , rgba(5, 25, 55, 0.74), rgba(0, 77, 122, 0.7), rgba(0, 135, 147, 0.71), rgba(0, 191, 114, 0.7), rgba(168, 235, 18, 0.7)');
 });
 $(".color-3").click(function () {
   $(".navbar").css('background-image', 'linear-gradient(to right, rgba(235, 153, 47, 0.7), rgba(236, 130, 28, 0.7), rgba(236, 104, 14, 0.7), rgba(236, 73, 11, 0.7), rgba(235, 18, 18, 0.7))');
   $(".sidenav-color").css('background-image', 'linear-gradient(to left bottom, rgba(235, 153, 47, 0.7), rgba(236, 130, 28, 0.7), rgba(236, 104, 14, 0.7), rgba(236, 73, 11, 0.7), rgba(235, 18, 18, 0.7))');
+  $(".bg-color-statusCard").css('background-image', 'linear-gradient(to right , rgba(235, 153, 47, 0.7), rgba(236, 130, 28, 0.7), rgba(236, 104, 14, 0.7), rgba(236, 73, 11, 0.7), rgba(235, 18, 18, 0.7))');
 });
 $(".color-4").click(function () {
   $(".navbar").css('background-image', 'linear-gradient(to right, rgba(47, 63, 235, 0.7), rgba(0, 119, 255, 0.7), rgba(0, 147, 227, 0.7), rgba(0, 166, 176, 0.7), rgba(40, 179, 137, 0.7))');
   $(".sidenav-color").css('background-image', 'linear-gradient(to left bottom, rgba(47, 63, 235, 0.7), rgba(0, 119, 255, 0.7), rgba(0, 147, 227, 0.7), rgba(0, 166, 176, 0.7), rgba(40, 179, 137, 0.7))');
+  $(".bg-color-statusCard").css('background-image', 'linear-gradient(to right , rgba(47, 63, 235, 0.7), rgba(0, 119, 255, 0.7), rgba(0, 147, 227, 0.7), rgba(0, 166, 176, 0.7), rgba(40, 179, 137, 0.7))');
 });
 
 
